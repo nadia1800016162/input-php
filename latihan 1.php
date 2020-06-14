14bceaea79945af8da7a859084b543289f6bd5a9
nadia-muliya <nadia1800016162@webmail.uad.ac.id>
2020-06-14 20:04:34 +0700

    input-php

---
+++ latihan 1.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Output</title>
+</head>
+
+<body>
+
+<table border="1" align=>
+<BR>
+
+<tr>
+	 <td width="117"><b>NIM</td>
+	 <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+	 <td><b>NAMA</td>
+	 <td><?php echo $_POST["NAMA"] ?></td>
+</tr>
+<tr>
+	 <td><b>Jenis Kelamin</td>
+	 <td><?php echo $_POST["KELAMIN"] ?></td>
+</tr>
+<tr>
+	 <td><b>Program Studi</td>
+	 <td><?php echo $_POST["PRODI"] ?></td>
+</tr>
+
+</table>
+</body>
+</html>
---
+++ latihan 2.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<table border="1" align=>
+<tr>
+	 <td width="117"><b>NIM</td>
+	 <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+	 <td><b>NAMA</td>
+	 <td><?php echo $_POST["NAMA"] ?></td>
+</tr>
+<tr>
+	 <td><b>Jenis Kelamin</td>
+	 <td><?php echo $_POST["KELAMIN"] ?></td>
+</tr>
+<tr>
+	 <td><b>Program Studi</td>
+	 <td><?php echo $_POST["PRODI"] ?></td>
+</tr>
+
+</table>
+</body>
+</html>
